4f51fa0c6645aedaa95ff4b910e46d8dfd3df909
azuma-ya <kazu.bluemountain@gmail.com>
2025-03-05 04:12:15 +0900

    init

---
+++ src/server.ts
@@ -0,0 +1,9 @@
+import { Origins, Server } from "boardgame.io/server";
+import { TicTacToe } from "./game/tic-tac-toe";
+
+const server = Server({
+  games: [TicTacToe],
+  origins: [Origins.LOCALHOST],
+});
+
+server.run(8000);
